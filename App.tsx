e5d3a5d374ebc7579dd2bc0972a41a9a7376c257
Rufi <43849574+shamiruf@users.noreply.github.com>
2022-06-14 23:40:44 +0200

    Show splash screen when app loading

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
-import { View, Text, Image, StatusBar } from 'react-native'
-import { NavigationContainer, DefaultTheme, RouteProp, ParamListBase } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import {View, Text, Image, StatusBar} from 'react-native'
+import {
+  NavigationContainer,
+  DefaultTheme,
+  RouteProp,
+  ParamListBase,
+} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 
 import ProposalScreen from './src/screens/proposal'
 import FeedScreen from './src/screens/feed'
@@ -11,6 +16,7 @@ import SearchScreen from './src/screens/search'
 import ProfileScreen from './src/screens/profile'
 import DAOScreen from './src/screens/dao'
 import FullProposalScreen from './src/screens/fullProposal'
+import SplashScreen from 'react-native-splash-screen'
 
 const feedIcon = require('./src/assets/icons/feed.png')
 const feedFocusedIcon = require('./src/assets/icons/feedFocused.png')
@@ -19,15 +25,13 @@ const profileFocusedIcon = require('./src/assets/icons/profileFocused.png')
 const searchIcon = require('./src/assets/icons/search.png')
 const searchFocusedIcon = require('./src/assets/icons/searchFocused.png')
 
-
 const navTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
     background: '#000000',
   },
-};
-
+}
 
 const Tab = createBottomTabNavigator()
 
@@ -38,21 +42,29 @@ const headerOptions = {
     backgroundColor: 'rgba(28, 28, 28, 0.9)',
   },
   headerTintColor: 'white',
-  headerBackTitleVisible: false
+  headerBackTitleVisible: false,
 }
 
 function HomeStackScreen() {
   return (
     <HomeStack.Navigator screenOptions={headerOptions}>
       <HomeStack.Screen name="Feed" component={FeedScreen} />
-      <HomeStack.Screen name="Proposal" component={ProposalScreen} options={{
-        headerTitle: 'Snapshot voting',
-      }}/>
-      <HomeStack.Screen name="FullProposal" component={FullProposalScreen} options={{
-        headerTitle: 'Snapshot voting',
-      }}/>
+      <HomeStack.Screen
+        name="Proposal"
+        component={ProposalScreen}
+        options={{
+          headerTitle: 'Snapshot voting',
+        }}
+      />
+      <HomeStack.Screen
+        name="FullProposal"
+        component={FullProposalScreen}
+        options={{
+          headerTitle: 'Snapshot voting',
+        }}
+      />
     </HomeStack.Navigator>
-  );
+  )
 }
 
 const ProfileStack = createNativeStackNavigator()
@@ -63,7 +75,7 @@ function ProfileStackScreen() {
       <ProfileStack.Screen name="Profile" component={ProfileScreen} />
       <ProfileStack.Screen name="DAO" component={DAOScreen} />
     </ProfileStack.Navigator>
-  );
+  )
 }
 
 const SearchStack = createNativeStackNavigator()
@@ -73,54 +85,72 @@ function SearchStackScreen() {
     <SearchStack.Navigator screenOptions={headerOptions}>
       <SearchStack.Screen name="Search" component={SearchScreen} />
     </SearchStack.Navigator>
-  );
+  )
 }
 
 export default function App() {
+  // show splash screen
+  React.useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle='light-content' />
+      <StatusBar barStyle="light-content" />
       <NavigationContainer theme={navTheme}>
-        <Tab.Navigator screenOptions={() => ({
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: '#151515',
-            borderTopColor: '#151515',
-          },
-        })}>
-          <Tab.Screen name="HomeStack" component={HomeStackScreen} options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ size, focused, color }) => {
-              return (
-                <Image
-                  style={{ width: size * 0.9, height: size * 0.9 }}
-                  source={focused ? feedFocusedIcon : feedIcon}
-                />
-              )
+        <Tab.Navigator
+          screenOptions={() => ({
+            headerShown: false,
+            tabBarStyle: {
+              backgroundColor: '#151515',
+              borderTopColor: '#151515',
             },
-          }} />
-          <Tab.Screen name="SearchStack" component={SearchStackScreen} options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ size, focused, color }) => {
-              return (
-                <Image
-                  style={{ width: size * 0.9, height: size * 0.9 }}
-                  source={focused ? searchFocusedIcon : searchIcon}
-                />
-              )
-            },
-          }} />
-          <Tab.Screen name="ProfileStack" component={ProfileStackScreen}  options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ size, focused, color }) => {
-              return (
-                <Image
-                  style={{ width: size * 0.9, height: size * 0.9 }}
-                  source={focused ? profileFocusedIcon : profileIcon}
-                />
-              )
-            },
-          }} />
+          })}>
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStackScreen}
+            options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({size, focused, color}) => {
+                return (
+                  <Image
+                    style={{width: size * 0.9, height: size * 0.9}}
+                    source={focused ? feedFocusedIcon : feedIcon}
+                  />
+                )
+              },
+            }}
+          />
+          <Tab.Screen
+            name="SearchStack"
+            component={SearchStackScreen}
+            options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({size, focused, color}) => {
+                return (
+                  <Image
+                    style={{width: size * 0.9, height: size * 0.9}}
+                    source={focused ? searchFocusedIcon : searchIcon}
+                  />
+                )
+              },
+            }}
+          />
+          <Tab.Screen
+            name="ProfileStack"
+            component={ProfileStackScreen}
+            options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({size, focused, color}) => {
+                return (
+                  <Image
+                    style={{width: size * 0.9, height: size * 0.9}}
+                    source={focused ? profileFocusedIcon : profileIcon}
+                  />
+                )
+              },
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react'
-import { Text, View, Alert, ScrollView, Image, ActivityIndicator } from 'react-native'
+import {Text, View, ScrollView, Image, ActivityIndicator} from 'react-native'
 import numeral from 'numeral'
 
-import { TPortfolioResponse } from '../../types'
-import { getPortfolio } from '../../services/api'
+import {TPortfolioResponse} from '../../types'
 
-const profileImage = require ('../../assets/images/profile.jpg')
+const profileImage = require('../../assets/images/profile.jpg')
 
 import styles from './styles'
 
 function ProfileScreen() {
-
   const [portfolio, setPortfolio] = React.useState<TPortfolioResponse>([])
   const [loading, setLoading] = React.useState<boolean>(false)
 
@@ -19,70 +17,74 @@ function ProfileScreen() {
   }, [])
 
   const fetchPortfolio = async () => {
-    console.log('Fetch')
-    try {
-      setLoading(true)
-      await getPortfolio().then(res => {
-        setPortfolio(res)
-        setLoading(false)
-      })
-    } catch (e: any) {
-      setLoading(false)
-      Alert.alert(
-        'Something went wrong',
-        'Please try again',
-        [
-          {
-            text: 'Reload',
-            onPress: fetchPortfolio
-          },
-          {
-            text: 'Cancel',
-            onPress: () => {}
-          },
-        ]
-      );
-    }
+    // console.log('Fetch')
+    // try {
+    //   setLoading(true)
+    //   await getPortfolio().then(res => {
+    //     setPortfolio(res)
+    //     setLoading(false)
+    //   })
+    // } catch (e: any) {
+    //   setLoading(false)
+    //   Alert.alert(
+    //     'Something went wrong',
+    //     'Please try again',
+    //     [
+    //       {
+    //         text: 'Reload',
+    //         onPress: fetchPortfolio
+    //       },
+    //       {
+    //         text: 'Cancel',
+    //         onPress: () => {}
+    //       },
+    //     ]
+    //   );
+    // }
   }
 
   return (
     <View style={styles.profileWrapper}>
-      {loading ? 
-      <View style={styles.loadingWrapper}>
-        <ActivityIndicator  size='large' color='#8463DF'/> 
-      </View>
-      :
-      
+      {loading ? (
+        <View style={styles.loadingWrapper}>
+          <ActivityIndicator size="large" color="#8463DF" />
+        </View>
+      ) : (
         <View>
           <View style={styles.profileInfoWrapper}>
-            <Image style={styles.profileImage} source={profileImage}/>
+            <Image style={styles.profileImage} source={profileImage} />
             <View style={styles.profileInfoTextWrapper}>
               <Text style={styles.profileName}>nakrmili.eth</Text>
-              <Text style={styles.profilePortfolioAmount}>You govern: {portfolio.length} DAOs</Text>
+              <Text style={styles.profilePortfolioAmount}>
+                You govern: {portfolio.length} DAOs
+              </Text>
             </View>
           </View>
           <Text style={styles.portfolioTitle}>Governance you follow</Text>
           <ScrollView style={styles.portfolioWrapper}>
-            {portfolio.map((asset) => (
+            {portfolio.map(asset => (
               <View key={asset.title} style={styles.assetWrapper}>
                 <View style={styles.assetLeft}>
-                  <Image style={styles.assetImage} source={{ uri: asset.icon}}/>
+                  <Image style={styles.assetImage} source={{uri: asset.icon}} />
                   <View style={styles.assetTextWrapper}>
                     <Text style={styles.assetTitle}>{asset.title}</Text>
-                    <Text style={styles.assetShare}>{+(asset.share * 100).toFixed(6)}% shares</Text>
+                    <Text style={styles.assetShare}>
+                      {+(asset.share * 100).toFixed(6)}% shares
+                    </Text>
                   </View>
                 </View>
                 <View style={styles.assetRight}>
-                  <Text style={styles.assetAmount}>{numeral(asset.amount).format('0[.]00')} {asset.symbol}</Text>
+                  <Text style={styles.assetAmount}>
+                    {numeral(asset.amount).format('0[.]00')} {asset.symbol}
+                  </Text>
                 </View>
               </View>
             ))}
           </ScrollView>
         </View>
-    }
+      )}
     </View>
   )
 }
-
 
 export default ProfileScreen
