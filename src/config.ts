62d73f9f30fe8dd63c2d805d5aa253eabe23b221
Rufi <43849574+shamiruf@users.noreply.github.com>
2022-06-23 15:02:06 +0200

    Requests in sepate file

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,2 @@
-import axios from 'axios'
-import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth'
-
 export const baseEndpoint: string =
   'https://holdim-api-v3ax6dvhgq-ey.a.run.app/v1'
-
-let headers: any = {}
-export const axiosInstance = axios.create({baseURL: baseEndpoint, headers})
-axiosInstance.interceptors.request.use(
-  async config => {
-    const user: FirebaseAuthTypes.User | null = auth().currentUser
-
-    if (user) {
-      const idTokenResult: FirebaseAuthTypes.IdTokenResult =
-        await user.getIdTokenResult()
-      if (config.headers) {
-        config.headers.Authorization = `User JWT ${idTokenResult.token}`
-      }
-    }
-
-    return config
-  },
-  error => {
-    return Promise.reject(error)
-  },
-)
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,41 @@
 import axios from 'axios'
-import { baseEndpoint } from '../config'
-import { TPortfolioResponse, TFeedResponse } from '../types'
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth'
 
-export const getPortfolio = (): Promise<TPortfolioResponse>  => {
-  return axios.get(`${baseEndpoint}/users/me`)
-    .then(res => res.data)
+import {baseEndpoint} from '../config'
+import {Alert} from 'react-native'
+
+let headers: any = {}
+
+export const axiosInstance = axios.create({baseURL: baseEndpoint, headers})
+axiosInstance.interceptors.request.use(
+  async config => {
+    const user: FirebaseAuthTypes.User | null = auth().currentUser
+
+    if (user) {
+      const idTokenResult: FirebaseAuthTypes.IdTokenResult =
+        await user.getIdTokenResult()
+      if (config.headers) {
+        config.headers.Authorization = `User JWT ${idTokenResult.token}`
+      }
+    }
+
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  },
+)
+
+// TODO add more variants for each type of error
+export const handleHTTPError = () => {
+  Alert.alert('Something went wrong', `Please try again`, [
+    {
+      text: 'Ok',
+    },
+  ])
 }
 
-export const getFeed = (): Promise<TFeedResponse>  => {
-  return axios.get(`${baseEndpoint}/feed`)
-    .then(res => res.data)
+// TODO change type (any)
+export const createUser = (walletAddressInput: string): Promise<any> => {
+  return axiosInstance.post('/user/register', walletAddressInput)
 }
