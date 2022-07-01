d492298b9889cd9c353616043d636be245bff51c
Rufi <43849574+shamiruf@users.noreply.github.com>
2022-07-01 13:04:23 +0200

    Middle proposal detail

--- src/screens/proposal/index.tsx
+++ src/screens/proposal/index.tsx
@@ -1,79 +1,145 @@
 import * as React from 'react'
-import { Text, View, ScrollView, Image, TouchableWithoutFeedback } from 'react-native'
+import {
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableWithoutFeedback,
+} from 'react-native'
 import moment from 'moment'
 import numeral from 'numeral'
-import { TFeedResponse } from '../../types'
 
+import {TPool, TProposal} from '../../types'
+import {convertURIForLogo} from '../feed'
 import styles from './styles'
 
-function ProposalScreen({ route, navigation }: any) {
-
-  const [proposal, setProposal] = React.useState<TFeedResponse[0]>()
-
-
-  React.useEffect(() => {
-    if (route.params?.proposal) {
-      setProposal(route.params.proposal)
-    }
-  }, [])
-
-  if (!proposal) return <View />
+function ProposalScreen({route, navigation}: any) {
+  const [proposal, setProposal] = React.useState<TProposal>(
+    route.params.proposal,
+  )
+  const [pool, setPool] = React.useState<TPool>(route.params.pool)
 
   const shortenAddress = (address: string) => {
     if (address.length <= 12) return address
-  
+
     const start = address.slice(0, 6)
     const end = address.slice(address.length - 4, address.length)
     const result = start + '...' + end
     return result
   }
 
-  const openFullProposal = (proposal: TFeedResponse[0]) => {
+  const openFullProposal = (proposal: TProposal) => {
     navigation.navigate('FullProposal', {proposal})
   }
-  
+
+  React.useEffect(() => {
+    if (route.params.proposal) {
+      setProposal(route.params.proposal)
+      setPool(route.params.pool)
+    }
+  }, [proposal])
 
   return (
     <ScrollView style={styles.proposalWrapper}>
-      {proposal ? 
+      {route.params.proposal ? (
         <View style={styles.proposalWrapper}>
-          <View  style={styles.proposalTopSectionWrapper}>
-            <Image style={styles.proposalIcon} source={{ uri: proposal.icon}}/>
-            <Text style={styles.proposalDaoTitle}>{proposal.daoTitle}</Text>
+          <View style={styles.proposalTopSectionWrapper}>
+            <Image
+              style={styles.proposalIcon}
+              source={{uri: convertURIForLogo(proposal.dao.logo)}}
+            />
+            <Text style={styles.proposalDaoTitle}>{proposal.dao.name}</Text>
           </View>
           <Text style={styles.proposalTitle}>{proposal.title}</Text>
-          <Text style={styles.proposalDescr}>TL:DR (AI translated)</Text>
-          <Text style={styles.proposalDescr}>{proposal.description.short}</Text>
+          <Text style={styles.proposalDescription}>TL:DR (AI translated)</Text>
+          <Text style={styles.proposalDescription}>
+            {proposal.middleDescription}
+          </Text>
           <TouchableWithoutFeedback onPress={() => openFullProposal(proposal)}>
             <View style={styles.proposalButton}>
-            <Text style={styles.proposalButtonText}>Read full version</Text>
+              <Text style={styles.proposalButtonText}>Read full version</Text>
             </View>
-            </TouchableWithoutFeedback>
+          </TouchableWithoutFeedback>
           <View style={styles.proposalMetaWrapper}>
-            <View style={styles.proposalMeta}><Text style={styles.proposalMetaTitle}>Starts:</Text><Text style={styles.proposalMetaInfo}>{moment.unix(proposal.start).format('MMM DD, YYYY, hh:mm')}</Text></View>
-            <View style={styles.proposalMeta}><Text style={styles.proposalMetaTitle}>End:</Text><Text style={styles.proposalMetaInfo}>{moment.unix(proposal.end).format('MMM DD, YYYY, hh:mm')}</Text></View>
-            <View style={styles.proposalMeta}><Text style={styles.proposalMetaTitle}>Author:</Text><Text style={styles.proposalMetaInfo}>{shortenAddress(proposal.author)}</Text></View>
-            <View style={styles.proposalMeta}><Text style={styles.proposalMetaTitle}>Total voters:</Text><Text style={styles.proposalMetaInfo}>{proposal.votes}</Text></View>
+            <View style={styles.proposalMeta}>
+              <Text style={styles.proposalMetaTitle}>Starts:</Text>
+              <Text style={styles.proposalMetaInfo}>
+                {moment(new Date(proposal.startAt)).format(
+                  'MMM DD, YYYY, HH:MM A',
+                )}
+              </Text>
+            </View>
+            <View style={styles.proposalMeta}>
+              <Text style={styles.proposalMetaTitle}>End:</Text>
+              <Text style={styles.proposalMetaInfo}>
+                {moment(new Date(proposal.endAt)).format(
+                  'MMM DD, YYYY, HH:MM A',
+                )}
+              </Text>
+            </View>
+            <View style={styles.proposalMeta}>
+              <Text style={styles.proposalMetaTitle}>Author:</Text>
+              <Text style={styles.proposalMetaInfo}>
+                {shortenAddress(proposal.author)}
+              </Text>
+            </View>
+            <View style={styles.proposalMeta}>
+              <Text style={styles.proposalMetaTitle}>Total voters:</Text>
+              <Text style={styles.proposalMetaInfo}>{pool && pool.votes}</Text>
+            </View>
           </View>
           <View style={styles.proposalVotingWrapper}>
-                {proposal.scores.map((score, i) => (
+            {route.params.pool &&
+              route.params.pool.choices &&
+              route.params.pool.choices.map(
+                (choiceTitle: string, i: number) => (
                   <View key={i} style={styles.proposalVotingItemWrapper}>
                     <View style={styles.proposalVotingItemTextWrapper}>
-                      <Text style={styles.proposalVotingItemText}>{score.title}</Text>
-                      <Text style={styles.proposalVotingItemText}>{numeral(score.votingPower).format('0[.]0a')} {+(score.share * 100).toFixed()}%</Text>
+                      <Text style={styles.proposalVotingItemText}>
+                        {choiceTitle}
+                      </Text>
+                      <Text style={styles.proposalVotingItemText}>
+                        {numeral(pool.scores[i]).format('0[.]0a')} {pool.symbol}
+                        {'  '}
+                        {
+                          +(
+                            (pool.scores[i] * 100) /
+                            pool.scores_total
+                          ).toFixed()
+                        }
+                        %
+                      </Text>
                     </View>
                     <View style={styles.proposalVotingItemBackgroundLine}>
-                      <View style={{...styles.proposalVotingItemInnerLine, backgroundColor: '#8463DF', width: `${score.share * 100}%`}} />
+                      <View
+                        style={{
+                          ...styles.proposalVotingItemInnerLine,
+                          backgroundColor: '#8463DF',
+                          width: `${
+                            (pool.scores[i] * 100) / pool.scores_total
+                          }%`,
+                        }}
+                      />
                     </View>
                   </View>
-                ))}
+                ),
+              )}
+            {pool && pool.quorum !== 0 && (
+              <View style={styles.proposalVotingItemTextWrapper}>
+                <Text style={styles.proposalVotingItemText}>Quorum</Text>
+                <Text style={styles.proposalVotingItemText}>
+                  {numeral(pool && pool.scores_total).format('0[.]0a')}/
+                  {numeral(pool && pool.quorum).format('0[.]0a')}
+                </Text>
               </View>
+            )}
+          </View>
         </View>
-      : <Text>No</Text>
-    }
+      ) : (
+        <Text style={{color: 'white'}}>No</Text>
+      )}
     </ScrollView>
   )
 }
-
 
 export default ProposalScreen
